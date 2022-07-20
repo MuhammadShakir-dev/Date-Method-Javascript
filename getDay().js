2875f9e73fe6118db8b0837ad7d2a6ace7ebedf1
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-20 13:52:19 +0500

    Update getDay().js

--- getDay().js
+++ getDay().js
@@ -1,1 +1,7 @@
+// it will give you the current day
+// 1 = monday and 0 = Sunday
+// 2 = tues 3 = wed 4 = thurs 5 = fri 6 = sat
 
+
+       var date = new Date();
+        document.write(date.getDay());
