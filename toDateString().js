73e58f8182b3d47a502f05c9286f79cbf73bf024
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-20 13:49:06 +0500

    Update toDateString().js

--- toDateString().js
+++ toDateString().js
@@ -1,1 +1,12 @@
 
+             /*
+                               Date methods in javascript
+                                 whats is Date() in javascript?
+                                    Date() is used to perform date operation on date.
+
+
+                                    for implementying Date() in javascript you need to initialize the Date() object.
+
+                */
+                                 var date = new Date();
+                                 document.write(date.toDateString());
